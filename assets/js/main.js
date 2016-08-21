17782d8cd5042857b2314d2caf951d0da8eee70d
FathanRohman <doctor.fathan@gmail.com>
2016-08-21 15:51:13 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,49 @@
+/* ---------------------------------
+Javascript(jQuery) for Apecsa Optima Solusi
+
+version: 2.0
+author : Fathan Rohman
+email  : karir.fathan@gmail.com
+website: http://apecsa-indonesia.com
+-----------------------------------*/
+
+$(document).ready(function () {
+	new WOW().init();
+	
+	// TOOLTIP
+	$('[data-toggle="tooltip"]').tooltip()
+	
+	// SCROLL TO FIXED
+	$('#header-microbiz').scrollToFixed();
+
+	// SET HEIGHT CAROUSEL HOME
+	$('.carousel-car .item').each(function(){
+		var topSlider = $('.carousel-car').outerWidth() / 2.4;
+		$(this).css('height', topSlider);
+	});
+
+	// BACK TO TOP
+	if ($('#back-to-top').length) {
+	    var scrollTrigger = 100, // px
+	    backToTop = function () {
+	    	var scrollTop = $(window).scrollTop();
+	    	if (scrollTop > scrollTrigger) {
+	    		$('#back-to-top').addClass('show');
+	    	} else {
+	    		$('#back-to-top').removeClass('show');
+	    	}
+	    };
+	    
+	    backToTop();
+	    $(window).on('scroll', function () {
+	    	backToTop();
+	    });
+
+	    $('#back-to-top').on('click', function (e) {
+	    	e.preventDefault();
+	    	$('html,body').animate({
+	    		scrollTop: 0
+	    	}, 700);
+	    });
+	}
+});
---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('styles', function () {
+  gulp.src('./assets/scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./assets/css'))
+    .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('serve', function () {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  });
+
+  gulp.watch('./assets/scss/**/*.scss', ['styles']);
+  gulp.watch('./**/*.html').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['styles', 'serve']);
