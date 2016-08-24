36aa21040a8d6ea7ea01464d07679e6c1ca08ef7
FathanRohman <doctor.fathan@gmail.com>
2016-08-24 07:29:16 +0700

    revision style

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
 	$('[data-toggle="tooltip"]').tooltip()
 	
 	// SCROLL TO FIXED
-	$('#navbar-apecsaos').scrollToFixed();
+	$('#navbar-fixed-apecsaos').scrollToFixed();
 
 	// SET HEIGHT CAROUSEL HOME
 	$('.carousel-car .item').each(function(){
